e8d82df96c83587feb41e8f94efaffb41c69fd5f
Betül Sakarya <betulsakarya05@gmail.com>
2019-06-16 18:33:37 +0300

    Create ListeÖrneği.py

---
+++ ListeÖrneği.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+liste=[]
+
+ad= input("Ad : ")
+soyad= input("Soyad : ")
+dogumYili=int(input("Dogum Yili : "))
+
+liste.append(ad)
+liste.append(soyad)
+liste.append(dogumYili)
+
+for i in liste:
+  print(i)
+
